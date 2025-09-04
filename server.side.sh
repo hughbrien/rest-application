d82f9b06277db48c57b68f5ba1853f29798a120f
hughbrien <hugh@hughbrien.com>
2025-09-04 11:09:17 -0400

    Updates

--- server.side.sh
+++
@@ -1,6 +0,0 @@
-
-
-nohup java -Dserver.port=8886 -javaagent:./splunk-otel-javaagent.jar -jar ./target/restapplication-0.0.4-SNAPSHOT.jar > app-side.log 2>&1 &
-
-
-nohup java -Dserver.port=8887 -javaagent:./splunk-otel-javaagent.jar -jar ./target/restapplication-0.0.4-SNAPSHOT.jar > server-side.log 2>&1 &
